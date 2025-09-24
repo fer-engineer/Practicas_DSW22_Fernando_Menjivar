09090782bc72db96853ffc47c62813e2ec5ec38d
menjivar-fer <menjivard762@gmail.com>
2025-09-24 17:28:52 -0600

    correct_println_messages

--- calculadora.kt
+++ calculadora.kt
@@ -1,26 +1,18 @@
+fun main() {
+    val num1 = 10
+    val num2 = 7
+    var resultado: Int? = null
 
-import multiplicacion
-import division
-fun main(){
-   val num1 = 10
-   val  num2 = 20
-   var resultado: Int? = null
-   /*var operador:String? = null  */
-
-
-    // Usamos la función suma creada más abajo
     resultado = suma(num1, num2)
-    println("El resultado de la suma es: $resultado") 
+    println("El resultado de la suma es: $resultado")
 
-     resultado = resta(num1, num2)
-    println("El resultado de la suma es: $resultado") 
+    resultado = resta(num1, num2)
+    println("El resultado de la resta es: $resultado")
 
     resultado = multiplicacion(num1, num2)
-    println("El resultado de la suma es: $resultado") 
+    println("El resultado de la multiplicacion es: $resultado")
 
-    println("El resultado de la suma es: ${division(num1, num2)}") 
-
-
+    println("El resultado de la division es: ${division(num1, num2)}")
 }
 
 fun suma(num1: Int, num2: Int): Int {
@@ -40,4 +32,3 @@ fun division(num1: Int, num2: Int): Double {
     val num2 = num2.toDouble()
     return num1 / num2
 }
-
